82e33b9176064ff0d4082a6c22070de7d742479e
Kirmirov <aruter@yandex.ru>
2023-07-19 23:09:27 +0300

    tests

--- main.js
+++ main.js
@@ -6,37 +6,65 @@
  * Добавить возможность выполнять этот скрипт через команду npm run tree -- path.
  */
 
-import fs from 'fs/promises';
-import path from 'path';
-
-// Получение пути из агрумента переданного после команды tree -- 
-const PATH_END = process.argv[2];
+const fs 	= require('fs/promises');
+const path 	= require('path');
 
 async function getTree(a_strPath) 
 {  
-	const TREE 			= { files: [], dirs: [] };
-	const DIRECTORY  	= path.dirname(a_strPath);
+	const TREE 				= { files: [], dirs: [] };
+	let strMainDirectory 	= '';
+
+	// Проверка переданного аргумента. Иначе выбрасываем ошибку:
+	try {
+		if ((await fs.stat(a_strPath)).isDirectory())
+		{
+			strMainDirectory  = path.dirname(a_strPath);
+
+			await walk(a_strPath);
+		}
+		else
+			return new Error('Unable to find a directory at the given path.');
+	} catch (err) {
+		if (err.code === 'ENOENT')
+			return new Error('The given path does not exist.');
+		else
+			return new Error("The given path is not a string.");
+	}
 
 	async function walk(a_strCurentPath)
 	{
-		// Проверка является ли файл папкой:
-		if ((await fs.stat(a_strCurentPath)).isDirectory())
-		{
-			// Записываем путь к папке:
-			TREE.dirs.push(path.relative(DIRECTORY, a_strCurentPath).replace(/\\/g, '/'));
-			// Получим ее содержимое:
-			let aDirectoryContent = await fs.readdir(a_strCurentPath);
-			// Проходимся по содержимому папки:
-			for (const strFileName of aDirectoryContent)
-				await walk(path.join(a_strCurentPath, strFileName));
+		// Записываем путь к папке в массив dirs:
+		TREE.dirs.push(path.relative(strMainDirectory, a_strCurentPath).replace(/\\/g, '/'));
+		// Получим ее содержимое:
+		let aDirectoryContent 	= await fs.readdir(a_strCurentPath);
+		// Обявляем массив для субдиректорий:
+		let aSubdirectories 	= [];
+		// Проходимся по содержимому папки:
+		for (const strFileName of aDirectoryContent) {
+			// Получаем полный путь до субдиректории:
+			let strFullPath = path.join(a_strCurentPath, strFileName);
+			// Заполняем массив субдиректорий, иначе добавляем путь к файлу в массив files:
+			if ((await fs.stat(strFullPath)).isDirectory())
+				aSubdirectories.push(strFullPath);
+			else 
+				TREE.files.push(path.relative(strMainDirectory, strFullPath).replace(/\\/g, '/'));
 		}
-		else
-			TREE.files.push(path.relative(DIRECTORY, a_strCurentPath).replace(/\\/g, '/'));
+		// Проходимся по массиву субдиректорий и перевызывем для них функцию walk:
+		for (const strSubdirPath of aSubdirectories)
+			await walk(strSubdirPath);
 	}
 
-	await walk(a_strPath);
-	console.log(TREE);
 	return TREE;
 }
 
-getTree(PATH_END);
+if (process.argv[2])
+{
+	// Получение пути из агрумента переданного после команды tree -- 
+	const PATH_END = process.argv[2];
+	getTree(PATH_END);
+}
+else
+	process.exit(0);
+
+
+module.exports = getTree;
--- main.test.js
+++ main.test.js
@@ -1,15 +1,53 @@
 const getTree 	= require('./main');
-const fs 		= require('fs/promises');
-const path 		= require('path');
+const mock 		= require('mock-fs');
 
-describe('getTree', () => {
-	it('should return an object with two arrays: files and dirs', () => {
-		const directoryPath = path.join(__dirname, 'test-directory');
-		const result = getTree(directoryPath);
-	
-		expect(result).toEqual({
-		  files: expect.any(Array),
-		  dirs: expect.any(Array),
+describe('Тестирование функции getTree', () => {
+	beforeAll(() => {
+		mock({
+			'path/to/dir/foo': {
+				'bar': {
+					'baz': {},
+					'bar1.txt': 'file bar1 content',
+					'bar2.txt': 'file bar2 content',
+				},
+				'f1.txt': 'file f1 content',
+				'f2.txt': 'file f2 content'
+			}
 		});
+	});
+
+	test('Функция должна вернуть ошибку, если переданный путь не существует', async () => {
+		const error = await getTree('path/to/nonexistent');
+		expect(error).toBeInstanceOf(Error);
+		expect(error.message).toBe('The given path does not exist.');
+	});
+	
+	test('Функция должна вернуть ошибку, если передан некорректный тип данных', async () => {
+		const error = await getTree(12345);
+		expect(error).toBeInstanceOf(Error);
+		expect(error.message).toBe('The given path is not a string.');
+	});
+
+	test('Функция должна вернуть ошибку если передан путь до файла', async () => {
+		const result = await getTree('path/to/dir/foo/f1.txt');
+		expect(result).toBeInstanceOf(Error);
+		expect(result.message).toBe('Unable to find a directory at the given path.');
+	});
+
+	test('Функция должна вернуть ожидаемый результат', async () => {
+		const pResult = {
+			files: [
+				'foo/f1.txt',
+				'foo/f2.txt',
+				'foo/bar/bar1.txt',
+				'foo/bar/bar2.txt',
+			],
+			dirs: ['foo', 'foo/bar', 'foo/bar/baz'],
+		};
+		expect(await getTree('path/to/dir/foo')).toStrictEqual(pResult);
+	});
+
+	afterAll(() => {
+		mock.restore();
 	});
 });
