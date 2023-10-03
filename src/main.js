c44498a7ed5471b3655c959b75aaf8caedc8003a
Kirmirov <aruter@yandex.ru>
2023-10-03 13:27:34 +0300

    start

--- src/main.js
+++ src/main.js
@@ -1,100 +1,37 @@
-import { LitElement, html, css } from "lit-element";
+/**
+ *  Задача: Напишите функцию tree(), которая принимает в качестве аргумента иерархический объект со 
+ * 	свойствами name, items и выводит структурированный вывод дерева в консоль.
+ */
 
-customElements.define("my-list", class MyList extends LitElement {
-	static styles = css`
-	h4 {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 60px;
-		width: 150px;
-		color: rgb(249 216 144);
-		background-color: rgb(58 189 73);
-		margin-left: 25px;
-		border-bottom-right-radius: 80%;
-		border-top-right-radius: 80%;
-		border-top-left-radius: 60%;
-		border-bottom-left-radius: 60%;
-	}
-  `;
+const pObject = {
+	"name": 1,
+	"items": [
+		{
+			"name": 2,
+			"items": [
+				{ "name": 3 }, 
+				{ "name": 4 }
+			]
+		}, 
+		{
+			"name": 5,
+			"items": [
+				{ "name": 6 }
+			]
+		}
+	]
+}
 
+const tree = (a_pObject, a_nLevel = 0) => {
 
-	static get properties() {
-		return {
-			id: { type: Number },
-		};
-	}
+	console.log(" ".repeat(a_nLevel * 4) + a_pObject.name);
 
-	render() {
-		return html`<h4 id="${this.id}">List id: ${this.id}</h4>`;
-	}
-});
-
-customElements.define("my-tree", class MyTree extends LitElement {
-	static styles = css`    
-    h3 {
-		color: green;
-		background-color: rgb(189 147 58);
-		width: 150px;
-		padding-left: 25px;
-		border-bottom-right-radius: 40%;
-		border-top-right-radius: 40%;
-		border-top-left-radius: 5px;
-    }
-	div {
-		margin-left: 25px;
-		position: relative;
-	}
-	div:before {
-		content: "";
-		position: absolute;
-		top: 0;
-		height: 100%;
-		width: 20px;
-		background-color: rgb(189 147 58);
-		border-top-left-radius: 5px;
-		border-bottom-right-radius: 5px;
-		border-bottom-left-radius: 5px;
-	}
-  `;
-	static get properties() {
-		return {
-			data: { type: Object },
-		};
-	}
-
-	attributeChangedCallback(name, oldVal, newVal) {
-		super.attributeChangedCallback(name, oldVal, newVal);
-	
-		if (name === 'data' && newVal !== null) {
-			this.data = JSON.parse(newVal);
+	if (Array.isArray(a_pObject.items)) {
+		for (const pItem of a_pObject.items) {
+			tree(pItem, a_nLevel + 1);
 		}
 	}
 
-	constructor() {
-		super();
-		this.data = {};
-	}
+}
 
-	render() {
-		return html`
-		<div id="${this.data.id}">
-			<h3>Branch id: ${this.data.id}</h3>
-			${this.renderData(this.data.items)}
-	  	</div>
-		`;
-	}
-
-	renderData(items) {
-		return items
-				? items.map(
-					(item) =>
-					html`${
-						item.items && item.items.length
-						? html`<my-tree data='${JSON.stringify(item)}'></my-tree>`
-						: html`<my-list id="${item.id}"></my-list>`
-					}`
-				)
-				: html``;
-	}
-});
+tree(pObject);
