a4c4d7658de6c75cc4cc9769ccd680179a8c084b
Kirmirov <aruter@yandex.ru>
2023-11-17 15:34:01 +0300

    new tree is done

--- main.js
+++ main.js
@@ -1,70 +1,62 @@
 /**
  * Задание :
- * Написать NodeJS скрипт tree для вывода списка файлов и папок файловой системы.
- * Результатом работы должен быть объект с массивами { files, folders }.
- * Вызовы файловой системы должны быть асинхронными. Скрипт принимает входной параметр - путь до папки.
- * Добавить возможность выполнять этот скрипт через команду npm run tree -- path.
+ * Напишите NodeJS скрипт tree для вывода списка файлов и папок файловой системы.
+ * Приложение должно принимать аргумент каталога для анализа и флаг --depth, -d с номером глубины 
+ * директорий. Результатом является вывод данных в структурированном виде дерева. 
+ * Вызовы файловой системы должны быть асинхронными.
  */
 
-const fs 	= require('fs/promises');
-const path 	= require('path');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const yargs = require('yargs');
 
-async function getTree(a_strPath) 
-{  
-	const TREE 				= { files: [], dirs: [] };
-	let strMainDirectory 	= '';
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
 
-	// Проверка переданного аргумента. Иначе выбрасываем ошибку:
-	try {
-		if ((await fs.stat(a_strPath)).isDirectory())
+
+const tree = async () => {
+
+	const createTree = async (strDir, nDepth) => {
+		const aFilesNames = await readdir(strDir);
+	
+		for (const strFileName of aFilesNames) 
 		{
-			strMainDirectory  = path.dirname(a_strPath);
-
-			await walk(a_strPath);
+			const strFilePath 	= path.join(strDir, strFileName);
+			const pFileStat 	= await stat(strFilePath);
+	
+			if (pFileStat.isDirectory() && nDepth > 0) 
+			{
+				console.log(`${'  '.repeat(nDepth - 1)}|-- ${strFileName}/`);
+				await generateTree(strFilePath, nDepth - 1);
+			} 
+			else
+				console.log(`${'  '.repeat(nDepth)}|-- ${strFileName}`);
 		}
-		else
-			return new Error('Unable to find a directory at the given path.');
-	} catch (err) {
-		if (err.code === 'ENOENT')
-			return new Error('The given path does not exist.');
-		else
-			return new Error("The given path is not a string.");
 	}
+	// аргументы ком. строки
+	const { argv } = yargs
+		.option('directory', {
+			alias: 'd',
+			demandOption: true,
+			type: 'string',
+		})
+		.option('depth', {
+			alias: 'depth',
+			type: 'number',
+			default: Infinity,
+		});
+  
+  	const { directory, depth } = argv;
 
-	async function walk(a_strCurentPath)
-	{
-		// Записываем путь к папке в массив dirs:
-		TREE.dirs.push(path.relative(strMainDirectory, a_strCurentPath).replace(/\\/g, '/'));
-		// Получим ее содержимое:
-		let aDirectoryContent 	= await fs.readdir(a_strCurentPath);
-		// Обявляем массив для субдиректорий:
-		let aSubdirectories 	= [];
-		// Проходимся по содержимому папки:
-		for (const strFileName of aDirectoryContent) {
-			// Получаем полный путь до субдиректории:
-			let strFullPath = path.join(a_strCurentPath, strFileName);
-			// Заполняем массив субдиректорий, иначе добавляем путь к файлу в массив files:
-			if ((await fs.stat(strFullPath)).isDirectory())
-				aSubdirectories.push(strFullPath);
-			else 
-				TREE.files.push(path.relative(strMainDirectory, strFullPath).replace(/\\/g, '/'));
-		}
-		// Проходимся по массиву субдиректорий и перевызывем для них функцию walk:
-		for (const strSubdirPath of aSubdirectories)
-			await walk(strSubdirPath);
+	try {
+		const strAbsolutePath = path.resolve(directory);
+		console.log(`Listing files and directories in: ${strAbsolutePath}\n`);
+		await createTree(strAbsolutePath, depth);
+	} catch (error) {
+		console.error('Error:', error.message);
 	}
-
-	return TREE;
-}
-
-if (process.argv[2])
-{
-	// Получение пути из агрумента переданного после команды tree -- 
-	const PATH_END = process.argv[2];
-	getTree(PATH_END);
-}
-else
-	process.exit(0);
+};
 
 
-module.exports = getTree;
+module.exports = { tree };
